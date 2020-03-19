022256b0b54bf3527ec75e1d991901903198aa96
Dawid Szwed <szwedzikpl@gmail.com>
2020-03-19 18:00:43 +0100

    Add peak memory usage to test summary; Add log file for future demo

--- test/index.php
+++ test/index.php
@@ -7,6 +7,10 @@
   $summary = $pboMission->export();
   $summary['parsingTime'] = (hrtime(true) - $startTime)/1e+6.' ms';
 
+  $memoryPeak = memory_get_peak_usage();
+  $unit=array('B','KB','MB','GB','TB','PB');
+  $summary['memoryPeakUsage'] = @round($memoryPeak/pow(1024,($i=floor(log($memoryPeak,1024)))),2).$unit[$i];
+
   print_r($summary);
 
   //echo json_encode($summary);
